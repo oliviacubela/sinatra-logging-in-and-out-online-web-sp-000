a2cdac15453f4e394eb50ae821644f93fd413415
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 16:21:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if self.current_user && self.is_logged_in?
+    if Helper.current_user && Helper.is_logged_in?
       erb :account
     end
     redirect '/error'
