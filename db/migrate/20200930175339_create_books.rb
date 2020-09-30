c389d01d7541422e047054014b5b72d6e65d2e65
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 17:58:01 +0000

    Automatically backed up by Learn

--- db/migrate/20200930175339_create_books.rb
+++ db/migrate/20200930175339_create_books.rb
@@ -1,6 +1,6 @@
-class CreateBooks < ActiveRecord::Migration[5.1]
+class CreatePosts < ActiveRecord::Migration[5.1]
   def change
-    create_table :books do |t|
+    create_table :posts do |t|
       t.string :name
       t.string :genre
     end
