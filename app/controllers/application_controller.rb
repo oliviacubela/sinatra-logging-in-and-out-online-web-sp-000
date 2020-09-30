8e782f0432a58d0b8e7f545f893c587faa5cc020
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 16:51:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,9 @@ class ApplicationController < Sinatra::Base
     if @user !=nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect '/account'
+    else
+      redirect '/error'
     end
-    redirect '/error'
   end
 
   get '/account' do
