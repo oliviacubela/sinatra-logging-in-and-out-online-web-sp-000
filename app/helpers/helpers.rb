34886fbc477d7f4e5e1e2058a565c25edcda2229
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 18:19:09 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 class Helpers
 
   def self.current_user(session)
-    User.find_by(session[:user_id])
+    User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
