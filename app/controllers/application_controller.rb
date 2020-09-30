d592d05bac8beb653bfbafc54c0ae41b88e78420
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 16:44:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     # if self.current_user && self.is_logged_in?
-    @user = User.find_by(username: params[:username], password: params[:password])
+    @user = User.find_by(username: params[:username])
     if @user
       session[:user_id] = @user.id
       redirect '/account'
