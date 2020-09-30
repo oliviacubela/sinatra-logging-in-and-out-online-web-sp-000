2f3b3bfb54e0c6cdb1d2623320f60635a599f667
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 18:24:16 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20200930175339) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.float "balance"
+    t.integer "balance"
   end
 
 end
