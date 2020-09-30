01eb474962be8dbe0be6db6bbccc360468e29af9
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 18:03:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200930175339) do
 
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "genre"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
