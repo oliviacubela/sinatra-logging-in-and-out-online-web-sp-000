c20c93e706af7757fbf3aeb54541b8da2535c9a2
Olivia Cubela <oliviacubela@gmail.com>
2020-09-30 16:50:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,12 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find_by(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
+  end
     erb :account
   end
 
